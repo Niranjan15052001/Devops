7f933b21f82df515ec941300504d16fe4ec363e0
Niranjan15052001 <niranjanuh155@gmail.com>
2023-01-06 18:27:31 +0530

    added another method writing Dockerfile

---
+++ flask_2/app.py
@@ -0,0 +1,14 @@
+from flask import Flask,render_template,request,redirect,url_for
+app = Flask(__name__)
+@app.route('/')
+def home():
+    return render_template('home.html')
+@app.route('/home',methods = ['POST','GET'])
+def index():
+    if request.method=='GET':
+        return '<h1>hey</h1>'
+    else:
+        return '<h2>hello</h2>'
+
+if __name__=='__main__':
+    app.run(host='0.0.0.0',port=3000)
